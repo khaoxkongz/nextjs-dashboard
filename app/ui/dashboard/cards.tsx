39b5e36d60b3798625c3baf9d93533bd832b2519
khaoxkongz <158423448+khaoxkongz@users.noreply.github.com>
2024-09-04 15:52:14 +0700

    ✨ feat: Fetching data for the dashboard overview page

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -1,10 +1,5 @@
-import {
-  BanknotesIcon,
-  ClockIcon,
-  UserGroupIcon,
-  InboxIcon,
-} from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import { BanknotesIcon, ClockIcon, InboxIcon, UserGroupIcon } from '@heroicons/react/24/outline';
 
 const iconMap = {
   collected: BanknotesIcon,
